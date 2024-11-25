456607d7368d87f0de1f36473ab33ab214a96e91
SairajRajput12 <125473711+SairajRajput12@users.noreply.github.com>
2024-11-25 06:13:19 +0530

    implemented concept of flexible components and created ui components

---
+++ src/Components/LoginForm.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import Form from '../UI/Form';
+import '../Components/SignupForm.css';
+import Button from '../UI/Button';
+
+
+export default function LoginForm() {
+  const SubmitForm = (e) => {
+        e.preventDefault()
+        console.log('Submit Form Function invoked'); 
+  }
+
+
+  return (
+    <>
+    <div className='signup-container'>
+    <h1>Testing: Login Form</h1>
+    <Form className='signup' onSubmit={SubmitForm}>
+        <label>Enter your password</label> 
+        <input type='password' /> 
+        <label>Enter your mail</label> 
+        <input type='email' />
+        <Button onSubmit={SubmitForm} className='submit-button'>Login</Button>
+    </Form>
+    </div>
+  </>
+  )
+}
---
+++ src/Components/SingupForm.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import Form from '../UI/Form';
+import '../Components/SignupForm.css';
+import Button from '../UI/Button';
+
+export default function SingupForm() {
+  const SubmitForm = (e) => {
+        e.preventDefault()
+        console.log('Submit Form Function invoked'); 
+  }
+
+
+  return (
+    <>
+      <div className='signup-container'>
+      <h1>User Signup Form</h1>
+      <Form className='signup' onSubmit={SubmitForm}>
+          <label>Enter your name</label>
+          <input type='text' /> 
+          <label>Enter your password</label> 
+          <input type='password' /> 
+          <label>Enter your confirm password</label>
+          <input type='password' />
+          <label>Enter your mail</label> 
+          <input type='email' />
+          <Button onSubmit={SubmitForm} className='submit-button'>Signup</Button>
+      </Form>
+      </div>
+    </>
+  )
+}
